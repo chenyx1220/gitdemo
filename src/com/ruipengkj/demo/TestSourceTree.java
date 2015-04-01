a37f4f66fe338d582a840cb4c84eb3e92232e8bd
Squall <13702239693@139.com>
2015-04-01 11:41:58 +0800

    保存修改

--- src/com/ruipengkj/demo/TestSourceTree.java
+++ src/com/ruipengkj/demo/TestSourceTree.java
@@ -1,11 +1,7 @@
 package com.ruipengkj.demo;
 
 public class TestSourceTree {
-	
-	private String mText1;
-	
-	public String getMTest() {
-		return mText1;
+
 	private String mText2;
 	
 	public String getMTest() {
