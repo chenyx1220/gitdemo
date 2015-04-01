5c37abc5ee6566c9fbcfe3846bf635fa7d648757
Squall <13702239693@139.com>
2015-04-01 10:59:59 +0800

    再冲突2

--- src/com/ruipengkj/demo/TestSourceTree.java
+++ src/com/ruipengkj/demo/TestSourceTree.java
@@ -2,10 +2,10 @@ package com.ruipengkj.demo;
 
 public class TestSourceTree {
 	
-	private String mText;
+	private String mText2;
 	
 	public String getMTest() {
-		return mText;
+		return mText2;
 	}
 
 }
